c52535fa3b8843b41a08dde4e79b908014dfe743
KIMHWI <whi951@ajou.ac.kr>
2021-07-06 14:56:59 +0900

    silver

--- basictest/덩치.py
+++ basictest/덩치.py
@@ -1,15 +1,15 @@
 testcase = int(input())
-count = 1
 big = [0,0]
 arr = [[0 for i in range(3)] for j in range(testcase)]
-for i in testcase:
-    arr[3] = 0
+for i in range(testcase):
+    arr[i][2] = 0
 for i in range(testcase):
     arr[i][0],arr[i][1] = map(int,input().split())
 for i in range(testcase):
+    count = 1
     for j in range(testcase):
-        if arr[i][0] > arr[j][0] and arr[i][1] > arr[j][0] and arr[i][3] != 0:
-            big = [arr[i][0],arr[i][1]]
-            
-
-print(",,",arr[1][0],arr[1][1])
+        if arr[i][0] < arr[j][0] and arr[i][1] < arr[j][1] and arr[i][2] == 0:
+            count+=1
+    arr[i][2] = count
+for i in range(testcase):
+    print(arr[i][2],end=' ')
