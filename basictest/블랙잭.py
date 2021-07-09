911dfa0bed23201be99b988153fdae8f8fc073cc
KIMHWI <whi951@ajou.ac.kr>
2021-07-09 09:08:20 +0900

    영화감독 슘

--- basictest/블랙잭.py
+++ basictest/블랙잭.py
@@ -1,3 +1,8 @@
 testcase,num = map(int,input().split())
 arr = list(map(int,input().split()))
+temp = 300000
+for i in range(testcase-1):
+    for j in range(1,testcase-1):
+        for k in range(2,testcase-1):
+            arr[i]+arr[j]+arr[k]
 print(testcase,num,arr)
---
+++ basictest/영화감독 숌.py
@@ -0,0 +1,12 @@
+testcase = int(input())
+count = 0
+num = 665
+while testcase > count:
+    num += 1
+    temp = str(num)
+    if "666" in temp:
+        count+=1    
+   
+
+    
+print(num)
