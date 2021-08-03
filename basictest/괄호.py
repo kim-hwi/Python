288faa475110536b610e92926ce4a94192b4b4c4
KIMHWI <whi951@ajou.ac.kr>
2021-08-03 09:20:54 +0900

    basic

--- basictest/괄호.py
+++ basictest/괄호.py
@@ -1,5 +1,17 @@
 testcase = int(input())
 arr=[]
+temp=[0]
 for i in range(testcase):
     arr.append(str(input()))
-print(arr)
+for i in range(testcase):
+    for j in range(len(arr[i])):
+        if temp[-1] == '(' and arr[i][j] ==')':
+            temp.pop()
+        else:
+            temp.append(arr[i][j])
+    if len(temp)==1:
+        print('YES')
+    else:
+        print('NO')
+    temp.clear()
+    temp=[0]
