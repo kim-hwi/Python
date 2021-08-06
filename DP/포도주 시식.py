f5e54cb986b71f340b9f3a82173c189650e3a289
KIMHWI <whi951@ajou.ac.kr>
2021-08-06 10:47:32 +0900

    DP

--- DP/포도주 시식.py
+++ DP/포도주 시식.py
@@ -1,55 +1,52 @@
 teatcase = int(input())
-arr = [[int(input()), 0, 0] for i in range(teatcase)]
+arr = [[int(input()), 0, 0,0,0] for i in range(teatcase)]
+
+
+
 
 def fun() :
         
+    
+    arr[0][4] = arr[0][0]
     if teatcase == 1:
-        arr[0][1] = arr[0][0]
         return
+    arr[1][1] = arr[0][0]+arr[1][0]
+    arr[1][4] = arr[1][1]
     if teatcase == 2:
-        arr[1][1] = arr[0][0]+arr[1][0]
         return
+    arr[2][1] = arr[0][0]+arr[1][0]
+    arr[2][2] = arr[1][0]+arr[2][0]
+    arr[2][3] = arr[0][0]+arr[2][0] 
+    arr[2][4] = max(arr[2][1],arr[2][2],arr[2][3])
     if teatcase == 3:
-        arr[2][1] = max( arr[0][0]+arr[1][0] , arr[0][0]+arr[2][0] , arr[1][0]+arr[2][0])
         return
-    arr[0][1] = arr[0][0]
-    arr[0][2] = 1
-    arr[1][1] = arr[1][0]+arr[0][1]
-    arr[1][2] = 2
 
-    for i in range(2, teatcase):
-        if arr[i-1][2] == 2:
+    for i in range(3, teatcase):
+        temp = i%3
+        if temp == 1:
+            arr[i][1] = arr[i-1][1]+arr[i][0]
+            arr[i][2] = arr[i-1][2]+arr[i][0]
+            arr[i][3] = arr[i-1][3]
+        if temp == 2:
+            arr[i][1] = arr[i-1][1]
+            arr[i][2] = arr[i-1][2]+arr[i][0]
+            arr[i][3] = arr[i-1][3]+arr[i][0]
+        if temp == 0:
+            arr[i][1] = arr[i-1][1]+arr[i][0]
+            arr[i][2] = arr[i-1][2]
+            arr[i][3] = arr[i-1][3]+arr[i][0]
 
-            if arr[i-1][1] < arr[i][0]+arr[i-1][0] :
-                arr[i][1] = arr[i][0]+arr[i-1][0]+arr[i-3][1]
-                arr[i][2] = 2
-                arr[i-1][2] = 1
-                arr[i-2][2] = 0
-
-            elif arr[i-1][1] <= arr[i][0]+arr[i-2][1]:
-                arr[i][1] = arr[i][0]+arr[i-2][1]
-                arr[i-1][2] = 0
-                arr[i][2] = 1
-            elif arr[i-1][1] > arr[i][0]+arr[i-1][0]:
-                arr[i][1] = arr[i-1][1]
-                arr[i][2] = 0
-
+        arr[i][4] = max(arr[i-1][4],
+                        arr[i-3][4] + arr[i-1][0] + arr[i][0], 
+                        arr[i-2][4] + arr[i][0])
         # 1 2 2
         # 1 3 5
         # 1 2 3 4
 
         # 1 2 1
-        # 5 6 8
-        # 5 1 3
+        # 5 6 6 6 9 12
+        # 3 3 0 0 3 3
 
-        elif arr[i-1][2] == 1:
-            arr[i][1] = arr[i][0]+arr[i-1][1]
-            arr[i][2] = 2
+fun()    
+print(arr[teatcase-1][4])
 
-        else:
-            arr[i][1] = arr[i][0]+arr[i-1][1]
-            arr[i][2] = 1
-    
-fun()
-print(arr)
-print(arr[teatcase-1][1])
