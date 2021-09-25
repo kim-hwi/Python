4fd21e33755494c94cf6a679c5db5483d9b30412
김휘 <whi951@ajou.ac.kr>
2021-09-25 23:48:25 +0900

    for 안에서 list copy하기

--- SW역량테스트/숫자만들기.py
+++ SW역량테스트/숫자만들기.py
@@ -1,32 +1,52 @@
-def dfs (temp,temp2):
-    # print(temp,temp2)
+def dfs(temp,temp2):
     global ans
+    # print(temp,temp2)
     if sum(temp) == 0:
         ans.append(temp2)
-    arr1 = temp.copy()
-    arr2 = temp.copy()
-    arr3 = temp.copy()
-    arr4 = temp.copy()
-    lis1 = temp2.copy()
-    lis2 = temp2.copy()
-    lis3 = temp2.copy()
-    lis4 = temp2.copy()
-    if arr1[0]>0:
-        arr1[0]-=1
-        lis1.append('+')
-        dfs(arr1,lis1)
-    if arr2[1] > 0:
-        arr2[1] -= 1
-        lis2.append('-')
-        dfs(arr2,lis2)
-    if arr3[2] > 0:
-        arr3[2] -= 1
-        lis3.append('*')
-        dfs(arr3,lis3)
-    if arr4[3] > 0:
-        arr4[3] -= 1
-        lis4.append('/')
-        dfs(arr4,lis4)
+    for i in range(4):
+        arr = temp.copy()
+        lis = temp2.copy()
+        if arr[i]>0:
+            arr[i]-=1
+            if i == 0:
+                lis.append('+')
+            if i == 1:
+                lis.append('-')
+            if i == 2:
+                lis.append('*')
+            if i == 3:
+                lis.append('/')
+            dfs(arr,lis)
+
+# def dfs (temp,temp2):
+#     # print(temp,temp2)
+#     global ans
+#     if sum(temp) == 0:
+#         ans.append(temp2)
+#     arr1 = temp.copy()
+#     arr2 = temp.copy()
+#     arr3 = temp.copy()
+#     arr4 = temp.copy()
+#     lis1 = temp2.copy()
+#     lis2 = temp2.copy()
+#     lis3 = temp2.copy()
+#     lis4 = temp2.copy()
+#     if arr1[0]>0:
+#         arr1[0]-=1
+#         lis1.append('+')
+#         dfs(arr1,lis1)
+#     if arr2[1] > 0:
+#         arr2[1] -= 1
+#         lis2.append('-')
+#         dfs(arr2,lis2)
+#     if arr3[2] > 0:
+#         arr3[2] -= 1
+#         lis3.append('*')
+#         dfs(arr3,lis3)
+#     if arr4[3] > 0:
+#         arr4[3] -= 1
+#         lis4.append('/')
+#         dfs(arr4,lis4)
 
 
 T = int(input())
